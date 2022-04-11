fe63e73633454376d01a1852650384a13712c375
Praneesha-Irugal <irugal96@gmail.com>
2022-04-11 14:56:59 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import { Navbar, Brand, CTA } from './components';
+import {Footer, Blog, Possibility, Features, WhatGPT3, Header} from './containers';
+//import things .. using this make code short and easy
+import './App.css';
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="gradient_bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+
+  )
+}
+
+export default App
